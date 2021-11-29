33365d98b9fc4c458e73246afd110235b4d5fee7
Patrick Hüntelmann <patrick.huentelmann@gmail.com>
2021-11-29 12:20:32 +0100

    implement person controller

--- backend/PfotenFreunde.Api/Controllers/PersonController.cs
+++ backend/PfotenFreunde.Api/Controllers/PersonController.cs
@@ -14,34 +14,57 @@ public class PersonController : ControllerBase
 	{
 		this.context = context;
 	}
+
+    /// <summary>
+    /// Gets all people
+    /// </summary>
+	[HttpGet]
+	public IEnumerable<Person> GetAll()
+	{
+		return context.People;
+	}
 	
     /// <summary>
     /// Gets information about the specified person
     /// </summary>
+    /// <response code="404">Person not found</response>
     [HttpGet("{id}")]
-    public IEnumerable<Person> Get(int id)
+    public async Task<ActionResult<Person>> Get(int id)
     {
-        // TODO: Implement this
-        return Enumerable.Empty<Person>();
+		var person = await this.context.People.FindAsync(id);
+
+		if (person == null)
+		{
+			return NotFound();
+		}
+
+		return person;
     }
 
     /// <summary>
     /// Adds a new person
     /// </summary>
     [HttpPost]
-    public void Post(Person institution)
+    public async Task Post(Person person)
     {
-        // TODO: Implement this
+        await this.context.AddAsync(person);
+		await this.context.SaveChangesAsync();
     }
 
     /// <summary>
     /// Updates information of the specified person
     /// </summary>
     /// <response code="403">Insufficient permissions</response>
+    /// <response code="404">Person not found</response>
     [HttpPatch("{id}")]
-    public void Patch(int id, Person institution)
+    public async Task<ActionResult> Patch(int id, Person person)
     {
-        // TODO: Implement this
+		person.Id = id;
+		this.context.People.Update(person);
+
+		await this.context.SaveChangesAsync();
+
+		return Ok();
     }
 
     /// <summary>
