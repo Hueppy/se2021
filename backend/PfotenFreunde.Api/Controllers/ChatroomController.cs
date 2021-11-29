501c04a3f855b88113cd412f28923bd7936e5ac5
Patrick Hüntelmann <patrick.huentelmann@gmail.com>
2021-11-29 13:14:13 +0100

    implement chatroom controller

--- backend/PfotenFreunde.Api/Controllers/ChatroomController.cs
+++ backend/PfotenFreunde.Api/Controllers/ChatroomController.cs
@@ -16,34 +16,69 @@ public class ChatroomController : ControllerBase
 	}
 	
     /// <summary>
+    /// Gets all chatrooms
+    /// </summary>
+    /// <response code="403">Insufficient permissions</response>
+    [HttpGet]
+    public IEnumerable<Chatroom> GetAll()
+    {
+		return this.context.Chatrooms;
+    }
+	
+    /// <summary>
     /// Get information of the specified chatroom
     /// </summary>
     /// <param name="id">Id of the chatroom</param>
     /// <response code="403">Insufficient permissions</response>
+    /// <response code="404">Chatroom not found</response>
     [HttpGet("{id}")]
-    public Chatroom Get(int id)
+    public async Task<ActionResult<Chatroom>> Get(int id)
     {
-        // TODO: Implement this
-        return null;
+		var chatroom = await this.context.Chatrooms.FindAsync(id);
+
+		if (chatroom == null)
+		{
+			return NotFound();
+		}
+
+		return chatroom;
     }
 
     /// <summary>
     /// Creates a new Chatroom
     /// </summary>
     [HttpPost]
-    public void Get(Chatroom chatroom)
+    public async Task<int> Post(Chatroom chatroom)
     {
-        // TODO: Implement this
+		var entry = await this.context.Chatrooms.AddAsync(chatroom);
+
+		await this.context.SaveChangesAsync();
+		
+		return entry.Entity.Id;
     }
 
     /// <summary>
     /// Updates information of a chatroom
     /// </summary>
     /// <response code="403">Insufficient permissions</response>
-    [HttpPatch]
-    public void Patch(Chatroom chatroom)
+    [HttpPatch("{id}")]
+    public async Task Patch(int id, Chatroom chatroom)
     {
-        // TODO: Implement this
+		chatroom.Id = id;
+		this.context.Chatrooms.Update(chatroom);
+
+		await this.context.SaveChangesAsync();
+    }
+
+	/// <summary>
+    /// Gets all messages of the chatroom
+    /// </summary>
+    /// <response code="403">Insufficient permissions</response>
+    /// <response code="404">Chatroom not found</response>
+    [HttpGet("{id}/messages")]
+    public async Task<IEnumerable<Message>> GetMessages(int id)
+    {
+		return this.context.Messages.Where(x => x.ChatroomId == id);
     }
 
     /// <summary>
@@ -51,8 +86,11 @@ public class ChatroomController : ControllerBase
     /// </summary>
     /// <response code="403">Insufficient permissions</response>
     [HttpPost("{id}/messages")]
-    public void PostMessage(int id, Message message)
+    public async Task PostMessage(int id, Message message)
     {
-        // TODO: Implement this
+		message.ChatroomId = id;
+		await this.context.Messages.AddAsync(message);
+
+		await this.context.SaveChangesAsync();
     }
 }
