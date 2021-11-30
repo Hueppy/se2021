f639322d78d827a19a52efa9e85478e9275e9b48
Patrick Hüntelmann <patrick.huentelmann@gmail.com>
2021-11-30 01:44:45 +0100

    prettify swagger documentation

--- backend/PfotenFreunde.Api/Program.cs
+++ backend/PfotenFreunde.Api/Program.cs
@@ -1,3 +1,5 @@
+using System.Reflection;
+using PfotenFreunde.Api.Filters;
 using PfotenFreunde.Shared.Contexts;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -8,7 +10,18 @@ builder.Services.AddControllers();
 builder.Services.AddDbContext<PfotenFreundeContext>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+	c.SupportNonNullableReferenceTypes();
+    
+    // Set the comments path for the Swagger JSON and UI.
+    var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+    c.IncludeXmlComments(xmlPath);
+
+	c.SchemaFilter<EnumMemberSchemaFilter>();
+	c.UseInlineDefinitionsForEnums();
+});
 
 var app = builder.Build();
 
